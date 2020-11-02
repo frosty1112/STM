0be5782ea896dbe8758d44508694c0a90eb72d9a
frosty1112 <73785440+frosty1112@users.noreply.github.com>
2020-11-01 20:02:13 -0500

    Add files via upload

---
+++ 3.3.py
@@ -0,0 +1,12 @@
+a=input("enter score")
+score = float(a)
+if (score >=0.9):
+    print("A");
+elif (score >=0.8):
+    print("B");
+elif(score >= 0.6):
+    print("c");
+elif(score<=0.6):
+    print("Fail");
+else:
+    print("error");
